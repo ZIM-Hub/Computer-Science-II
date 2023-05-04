325c9937a35d2dc46b7480ac7b79b7824c74773f
Jaime Ramirez <74738042+ZIM-Hub@users.noreply.github.com>
2023-05-04 17:57:17 -0600

    Create 1.24.1 Input and formatted output House real estate summary.c

---
+++ Chapter 1/1.24.1 Input and formatted output House real estate summary.c
@@ -0,0 +1,17 @@
+//1.24.1 Input and formatted output House real estate summary
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int currentPrice;
+   int lastMonthsPrice;
+   
+   cin >> currentPrice;
+   cin >> lastMonthsPrice;
+
+   cout << "This house is " << "$"<< currentPrice << "." <<" The change is " << "$" << currentPrice - lastMonthsPrice << " since last month." << endl;
+   cout << "The estimated monthly mortgage is " << "$" << (currentPrice * 0.051) / 12 << "." << endl;
+
+   return 0;
+}
