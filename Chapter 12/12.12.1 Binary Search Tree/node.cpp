64f76274b0803d6b18ae12487a4e467cfc8b1ba0
Jaime Ramirez <74738042+ZIM-Hub@users.noreply.github.com>
2023-05-04 20:16:24 -0600

    Update node.cpp

--- Chapter 12/12.12.1 Binary Search Tree/node.cpp
+++ Chapter 12/12.12.1 Binary Search Tree/node.cpp
@@ -1,17 +1,62 @@
+
+#include "Node.h"
 #include <iostream>
 
-class Node {
-    public:
-		int data;
-		Node* left;
-		Node* right;
+using namespace std;
 
-		Node(int data, Node* left=nullptr, Node* right=nullptr);
+Node::Node(int t_data, Node* t_left, Node* t_right) {
+	data = t_data;
+	left = t_left;
+	right = t_right;
+}
 
-		bool isLeaf();
-		void insert(int data);
-		bool isFull();
-		uint32_t height();
-		uint32_t countNodes();
+void Node::insert(int new_data) {
+	if (new_data < data) {
+		if (left == nullptr) {
+			left = new Node(new_data);
+		} else {
+			left->insert(new_data);
+		}
+	} else {
+		if (right == nullptr) {
+			right = new Node(new_data);
+		} else {
+			right->insert(new_data);
+		}
+	}
+}
 
-};
+bool Node::isLeaf() {
+	return left == nullptr && right == nullptr;
+}
+
+uint32_t Node::height() {
+	if (isLeaf()) {
+		return 0;
+	}
+	uint32_t leftHeight = left == nullptr ? 0 : left->height();
+	uint32_t rightHeight = right == nullptr ? 0 : right->height();
+	return 1 + max(leftHeight, rightHeight);
+}
+
+bool Node::isFull(){
+	if (left == nullptr && right == nullptr) {
+		return true;
+	}
+	else if (left != nullptr && right != nullptr) {
+		return left->isFull() && right->isFull();
+	}
+	else {
+		return false;
+	}
+}
+uint32_t Node::countNodes() {
+	uint32_t count = 1;
+	if (left != nullptr) {
+		count += left->countNodes();
+	}
+	if (right != nullptr) {
+		count += right->countNodes();
+	}
+	return count;
+}
